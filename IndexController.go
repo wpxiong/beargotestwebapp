2dbace99b026707d59a824d274f1057b077a7d29
wpxiong <wpxiong@gmail.com>
2016-03-13 15:59:48 +0900

    commit

--- IndexController.go
+++ IndexController.go
@@ -4,6 +4,7 @@ import (
   "github.com/wpxiong/beargo/log"
   "github.com/wpxiong/beargo/controller"
   "github.com/wpxiong/beargo/appcontext"
+  "github.com/wpxiong/beargo/session"
 )
 
 func init() {
@@ -19,12 +20,20 @@ type IndexControl struct {
   controller.Controller
 }
 
-func (*IndexControl) Before(ctx *appcontext.AppContext,form interface{}) bool {
+func (this *IndexControl) Before(ctx *appcontext.AppContext,form interface{}) bool {
      log.Debug("Test Before")
      return true
 }
 
-func (*IndexControl) Index(ctx *appcontext.AppContext,form interface{}){
+func (this *IndexControl) Index(ctx *appcontext.AppContext,form interface{}){
      indexform := form.(*Indexform)
+     request := ctx.Request.HttpRequest
+     response := ctx.Writer.HttpResponseWriter
+     var sess session.Session = session.NewSession(request , *response)
+     log.Debug("Index Method")
+     log.Debug(sess)
+     if nil == sess.GetSessionValue("text") {
+         sess.SaveSessionValue("test","xiongwenping")
+     }
      log.Debug(indexform)
 }

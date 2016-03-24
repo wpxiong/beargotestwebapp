f61e3ec01222d46e4a2459b52e8afc66b84ab8f3
xiong wenping <wp.xiong@gmail.com>
2016-03-24 18:57:07 +0900

    commit

--- main.go
+++ main.go
@@ -4,14 +4,37 @@ import (
   "github.com/wpxiong/beargo/log"
   "github.com/wpxiong/beargo/webapp"
   "github.com/wpxiong/beargo/appcontext"
+  "github.com/wpxiong/beargo/moudle"
+  "time"
   "runtime"
 )
 
+type AddInfo struct {
+  Address string  `length:"12"`
+  Id      int `id:"true"`
+}
+
+type UserInfo struct {
+  Id      int `id:"true"`
+  UserName string   `id:"true"     notnull:"true"     length:"128"     default_value:"testping"`
+  UserAge  uint16  `default_value:"54"`
+  UserSex  bool  `default_value:"true"`
+  Test     complex64 `default_value:"12.34,11.78"`
+  Add      []AddInfo
+  CreateTime  time.Time 
+}
 
 
 func main() {
    log.InitLogWithLevel("Debug")
    configMap := InitConfig()
+   
+   moudleInstance :=  moudle.CreateModuleInstance(moudle.MYSQL,"test","tcp(localhost:3306)","","")
+   
+   moudleInstance.AddTable(UserInfo{})
+   moudleInstance.AddTable(AddInfo{})
+   
+   moudleInstance.InitialDB(true)
    
    runtime.GOMAXPROCS(runtime.NumCPU())
    config := appcontext.AppConfigContext{Port :9001,ConfigPath : "./setting.conf"}

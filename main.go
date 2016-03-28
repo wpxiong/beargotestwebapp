48108f16eef99068f4f2f2f9a951f565e9828de9
xiong wenping <wp.xiong@gmail.com>
2016-03-28 15:23:59 +0900

    commit

--- main.go
+++ main.go
@@ -4,15 +4,60 @@ import (
   "github.com/wpxiong/beargo/log"
   "github.com/wpxiong/beargo/webapp"
   "github.com/wpxiong/beargo/appcontext"
+  "github.com/wpxiong/beargo/moudle"
+  "time"
   "runtime"
 )
 
+
+
+type AddressInformation struct {
+  Addressid int `id:"true"`
+  Userid int
+  AddressName string  `notnull:"true"     length:"128" `
+}
+
+type ClassInfo struct {
+  ClassName string `length:"128"`
+  ClassId int `id:"true"   auto_increment:"true"`
+  Address  string `length:"128"  notnull:"true"`
+  Goup    []GroupInfo `relation_type:"onetomany"  column_name:"classid" referenced_column_name:"classid"`
+}
+
+
+type GroupInfo struct {
+  UserId  int  `id:"true"`
+  ClassId int  `id:"true"`
+  Class ClassInfo  `relation_type:"manytoone" column_name:"classid"  referenced_column_name:"classid"`
+  User  UserInfo  `relation_type:"manytoone"   column_name:"userid" referenced_column_name:"id"`
+}
+
+
+type UserInfo struct {
+  Id      int `id:"true"`
+  UserName string   `id:"true"     notnull:"true"     length:"128"     default_value:"testping"`
+  UserAge  uint16  `default_value:"54"`
+  UserSex  bool  `default_value:"true"`
+  Test     complex64 `default_value:"12.34,11.78"`
+  CreateTime  time.Time
+  Addressid  int
+  Goup    []GroupInfo `relation_type:"onetomany" column_name:"id" referenced_column_name:"userid"`
+  Address  AddressInformation `relation_type:"onetoone" column_name:"addressid" referenced_column_name:"addressid"`
+}
 
 
 func main() {
    log.InitLogWithLevel("Debug")
    configMap := InitConfig()
    
+   moudleInstance :=  moudle.CreateModuleInstance(moudle.MYSQL,"test","tcp(localhost:3306)","","") 
+   
+   moudleInstance.AddTable(UserInfo{})
+   moudleInstance.AddTable(ClassInfo{})
+   moudleInstance.AddTable(GroupInfo{})
+   moudleInstance.AddTable(AddressInformation{})
+
+   moudleInstance.InitialDB(true)
    runtime.GOMAXPROCS(runtime.NumCPU())
    config := appcontext.AppConfigContext{Port :9001,ConfigPath : "./setting.conf"}
    

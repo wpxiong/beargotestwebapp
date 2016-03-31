68a62576b0b18a314609e4e45e3d9b9474fa7c56
wpxiong <wpxiong@gmail.com>
2016-04-01 00:46:51 +0900

    commit

--- main.go
+++ main.go
@@ -57,35 +57,37 @@ func main() {
    moudleInstance.AddTable(GroupInfo{})
    moudleInstance.AddTable(AddressInformation{})
 
-   moudleInstance.InitialDB(false)
-   /*moudleInstance.Insert(AddressInformation{Addressid:1,Userid:12,AddressName:"tokyo shibuya"})
+   moudleInstance.InitialDB(true)
+   var inserInfo *moudle.DbSqlBuilder = moudleInstance.Insert(AddressInformation{Addressid:1,Userid:12,AddressName:"tokyo shibuya"})
+   inserInfo.InsertExecute()
    userinfo := UserInfo{Addressid:1,UserName:"xiong",UserAge:22,UserSex:true,Test:complex(23.4,56.7),CreateTime:time.Now(),Id:1}
-    moudleInstance.Insert(AddressInformation{Addressid:2,Userid:12,AddressName:"tokyo shibuya"})
-   moudleInstance.Insert(userinfo)
+   
+   moudleInstance.Insert(AddressInformation{Addressid:2,Userid:12,AddressName:"tokyo shibuya"}).InsertExecute()
+   moudleInstance.Insert(userinfo).InsertExecute()
    userinfo.UserName = "baili"
    userinfo.Addressid = 2
    userinfo.Id = 2
-   moudleInstance.Insert(userinfo)
+   moudleInstance.Insert(userinfo).InsertExecute()
    
-   moudleInstance.Insert(ClassInfo{ClassName:"class1",Address:"address1"})
-   moudleInstance.Insert(ClassInfo{ClassName:"class2",Address:"address2"})
+   moudleInstance.Insert(ClassInfo{ClassName:"class1",Address:"address1"}).InsertExecute()
+   moudleInstance.Insert(ClassInfo{ClassName:"class2",Address:"address2"}).InsertExecute()
    
    userinfo.UserAge = 45
    userinfo.UserSex = false
    userinfo.UserName = "test"
-   moudleInstance.Update(userinfo)
+   moudleInstance.Update(userinfo).SaveExecute()
    userinfo.UserAge = 46
    userinfo.Test = complex(23.4,563.7)
-   moudleInstance.UpdateWithField(userinfo,[]string{"Test"})
+   moudleInstance.UpdateWithField(userinfo,[]string{"Test"}).SaveExecute()
    
-   moudleInstance.Insert(GroupInfo{UserId:1,ClassId:1})
-   moudleInstance.Insert(GroupInfo{UserId:1,ClassId:2})
-   moudleInstance.Insert(GroupInfo{UserId:2,ClassId:1})
-   moudleInstance.Insert(GroupInfo{UserId:2,ClassId:2}) */
+   moudleInstance.Insert(GroupInfo{UserId:1,ClassId:1}).InsertExecute()
+   moudleInstance.Insert(GroupInfo{UserId:1,ClassId:2}).InsertExecute()
+   moudleInstance.Insert(GroupInfo{UserId:2,ClassId:1}).InsertExecute()
+   moudleInstance.Insert(GroupInfo{UserId:2,ClassId:2}).InsertExecute()
    
-   var info moudle.QueryInfo  = moudleInstance.Query(UserInfo{},moudle.EAGER,[]string{"Goup","Address"})
-   //var info moudle.QueryInfo  = moudleInstance.SimpleQuery(GroupInfo{})
-   list := info.GetResultList()
+   var info *moudle.DbSqlBuilder  = moudleInstance.Query(UserInfo{},moudle.EAGER,[]string{"Goup","Address"})
+   //var info moudle.DbSqlBuilder  = moudleInstance.SimpleQuery(GroupInfo{})
+   list := info.FetchAll()
    for _,val := range list {
       usr := val.(UserInfo)
       log.Debug(usr)
